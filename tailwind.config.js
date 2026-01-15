12b1d2d7e3344ef64d7db34af17e6518759170e4
aloshhen <166549924+aloshhen@users.noreply.github.com>
2026-01-15 15:06:15 +0100

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'news-dark': '#0a0a0a',
+      },
+    },
   },
   plugins: [],
 }
